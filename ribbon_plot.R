e7c6636a28a5933d8e92bd5b02a278847e1a87a7
Linden Wells <lindenwells9@gmail.com>
2021-06-25 12:20:09 +1000

    Clean up ribbon_plot

--- ribbon_plot.R
+++ ribbon_plot.R
@@ -1,22 +1,36 @@
-setwd("~/Linden/Soil Temperature")
+setwd("~/Linden/Soil Temperature/apsimData")
+library(tidyverse)
+library(lubridate)
+
+read.met <- function(met.name) {
+  header<-readLines(met.name, n=25)
+  i<-grep("^year*|^Date*", trimws(header), ignore.case=T)
+  if (i <= 0) {return(NULL)}
+  met <- read.table(met.name,skip=i+2,na.strings=c("NA","?"))
+  names(met) <- unlist(strsplit(trimws(header[i]), " +"))
+  return(met)
+}
 
 # Daily
-ddf<-do.call(rbind, lapply(list.files(path="apsimData", "^Sorghum_[0-9].*Daily.out$"), read.met))
+ddf<-do.call(rbind, lapply(list.files(path=".", "^Sorghum_[0-9].*Daily.out$"), read.met))
 ddf$Date <- as.Date(ddf$Date,"%d/%m/%Y")
 ddf$year<-as.numeric(format.Date(ddf$Date, format="%Y"))
 ddf$src<- factor(ifelse(ddf$emember=="obs" , "Obs", "Pred"), levels=c("Obs", "Pred"))
 ddf$daysFromStart <- as.integer(
   as.Date(paste0("2001/", format.Date(ddf$Date, format="%m/%d")),"%Y/%m/%d")
   - as.Date(paste0("2001-", ddf$sowdate), "%Y-%d-%b"))
+ddf$sowdate <- factor(ddf$sowdate, levels = c("15-jun", "1-jul", "15-jul", "1-aug","15-aug"))
 
 thisYear<-1994
-thisSite<- "surat"
+thisSite<- "dubbo"
 thisSowdate<-"15-aug"
 
-relevant.ddf <- filter(ddf, year == thisYear & site == thisSite & daysFromStart <= 22 & sowdate == thisSowdate)
-
-ggplot(data = relevant.ddf) +
-  geom_boxplot(mapping = aes(daysFromStart, soil_temp_1, group = daysFromStart), color = "green") +
-  geom_boxplot(mapping = aes(daysFromStart, soil_mint_1, group = daysFromStart), color = "blue") +
-  facet_grid(.~sowdate)
+plot_ribbon <- function(ddf, date, site, period) {#period :: Int, site :: String, date :: Date, ddf :: data.frame
+  filter(ddf, between(Date, date, date + days(period)), site == thisSite) %>%
+  ggplot() +
+    geom_smooth(stat = 'summary', alpha = 0.5, fill = "gray", mapping = aes(daysFromStart, soil_mint_1),
+                fun.data = median_hilow, fun.args = list(conf.int = 0.5))
+}
+#plot the median min temp as a line, and the IQR as the grey shaded area.
+  
 
