de7dbdcdfb9993b89092c9574dfaafb61fd2bbdd
Linden Wells <lindenwells9@gmail.com>
2021-06-25 14:57:01 +1000

    Add reactive plot! Blistering progress for once. That shiny video on RStudio's website was rly handy.

--- app.R
+++ app.R
@@ -1,8 +1,10 @@
 library(shiny)
 library(tidyverse)
+source("ribbon_plot.R")
+#This code assumes a data frame called ddf is in the environment already... FIXME
 
-plot_ribbon <- function(ddf, date, site, period) {#period :: Int, site :: String, date :: Date, ddf :: data.frame
-  filter(ddf, between(Date, date, date + days(period)), site == thisSite) %>%
+plot_ribbon <- function(ddf, date, siteLoc, period) {#period :: Int, site :: String, date :: Date, ddf :: data.frame
+  filter(ddf, between(Date, date, date + days(period)), site == siteLoc) %>%
     ggplot() +
     geom_smooth(stat = 'summary', alpha = 0.5, fill = "gray", mapping = aes(daysFromStart, soil_mint_1),
                 fun.data = median_hilow, fun.args = list(conf.int = 0.5))
@@ -12,14 +14,15 @@ plot_ribbon <- function(ddf, date, site, period) {#period :: Int, site :: String
 ui <- fluidPage("Predicting Soil Temps",
                 selectInput("site", label="Choose a site",
                             c("breeza", "dalby", "dubbo","emerald", "moree", "surat", "warra")),
-                dateInput("date", label="Choose a date to start at"),
+                dateInput("date", label="Choose a date to start at", value = "1990-06-16"),
                 sliderInput("period", "Forecast for how many days?", 7, 21, 14, round = TRUE),
-                textOutput("result"))
+                textOutput("result"),
+                plotOutput("plot"))
 
 server <- function(input, output) {
   
-  output$result <- renderText({
-    paste("You chose", input$site)
+  output$plot <- renderPlot({
+    plot_ribbon(ddf, input$date, input$site, input$period)
   })
   
 }
--- ribbon_plot.R
+++ ribbon_plot.R
@@ -12,21 +12,21 @@ read.met <- function(met.name) {
 }
 
 # Daily
-ddf<-do.call(rbind, lapply(list.files(path=".", "^Sorghum_[0-9].*Daily.out$"), read.met))
-ddf$Date <- as.Date(ddf$Date,"%d/%m/%Y")
-ddf$year<-as.numeric(format.Date(ddf$Date, format="%Y"))
-ddf$src<- factor(ifelse(ddf$emember=="obs" , "Obs", "Pred"), levels=c("Obs", "Pred"))
-ddf$daysFromStart <- as.integer(
-  as.Date(paste0("2001/", format.Date(ddf$Date, format="%m/%d")),"%Y/%m/%d")
-  - as.Date(paste0("2001-", ddf$sowdate), "%Y-%d-%b"))
-ddf$sowdate <- factor(ddf$sowdate, levels = c("15-jun", "1-jul", "15-jul", "1-aug","15-aug"))
+# ddf<-do.call(rbind, lapply(list.files(path=".", "^Sorghum_[0-9].*Daily.out$"), read.met))
+# ddf$Date <- as.Date(ddf$Date,"%d/%m/%Y")
+# ddf$year<-as.numeric(format.Date(ddf$Date, format="%Y"))
+# ddf$src<- factor(ifelse(ddf$emember=="obs" , "Obs", "Pred"), levels=c("Obs", "Pred"))
+# ddf$daysFromStart <- as.integer(
+#   as.Date(paste0("2001/", format.Date(ddf$Date, format="%m/%d")),"%Y/%m/%d")
+#   - as.Date(paste0("2001-", ddf$sowdate), "%Y-%d-%b"))
+# ddf$sowdate <- factor(ddf$sowdate, levels = c("15-jun", "1-jul", "15-jul", "1-aug","15-aug"))
 
 thisYear<-1994
 thisSite<- "dubbo"
 thisSowdate<-"15-aug"
 
-plot_ribbon <- function(ddf, date, site, period) {#period :: Int, site :: String, date :: Date, ddf :: data.frame
-  filter(ddf, between(Date, date, date + days(period)), site == thisSite) %>%
+plot_ribbon <- function(ddf, date, siteLoc, period) {#period :: Int, site :: String, date :: Date, ddf :: data.frame
+  filter(ddf, between(Date, date, date + days(period)), site == siteLoc) %>%
   ggplot() +
     geom_smooth(stat = 'summary', alpha = 0.5, fill = "gray", mapping = aes(daysFromStart, soil_mint_1),
                 fun.data = median_hilow, fun.args = list(conf.int = 0.5))
