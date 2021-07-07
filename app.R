4b88b0edce4c50434a28a736c04278e436efd3a6
Linden Wells <lindenwells9@gmail.com>
2021-07-07 17:18:29 +1000

    Style cleanup (including changing axis labels). Add pipeline, a rough sketch of the data pipeline the app will follow.

--- app.R
+++ app.R
@@ -8,17 +8,31 @@ plot_ribbon <- function(df, date, siteLoc, period) {#period :: Int, site :: Stri
   filter(df, between(Date, date, date + days(period)), site == siteLoc) %>%
     ggplot() +
     geom_smooth(stat = 'summary', alpha = 0.5, fill = "gray", mapping = aes(daysFromStart, soil_mint_1),
-                fun.data = median_hilow, fun.args = list(conf.int = 0.5))
+                fun.data = median_hilow, fun.args = list(conf.int = 0.5)) +
+    xlab("days from start") +
+    
+    ylab("minimum soil temperature (°C)") +
+    
+    scale_x_continuous(n.breaks = period)
 }
 
 
-ui <- fluidPage("Predicting Soil Temps",
-                selectInput("site", label="Choose a site",
-                            c("breeza", "dalby", "dubbo","emerald", "moree", "surat", "warra")),
-                dateInput("date", label="Choose a date to start at", value = "1990-06-16"),
-                sliderInput("period", "Forecast for how many days?", 7, 21, 14, round = TRUE),
-                textOutput("result"),
-                plotOutput("plot"))
+ui <- fluidPage(h1("Predicting Soil Temps"),
+                
+                wellPanel(
+                  selectInput("site", label="Choose a site",
+                              c("breeza", "dalby", "dubbo","emerald", "moree", "surat", "warra")),
+                  dateInput("date", label="Choose a date to start at", value = "1990-06-16"),
+                  sliderInput("period", "Forecast for how many days?", 7, 21, 14, round = TRUE)
+                ),
+                
+                plotOutput("plot"),
+                p("Blue line is the median of predicted soil min temp"),
+                p("Grey area is the interquartile range of predicted soil min temp"),
+                
+                tags$footer(
+                  tags$a(href = "https://github.com/lindenwells/winter-research", "View this project on GitHub"))
+                )
 
 server <- function(input, output) {
   
