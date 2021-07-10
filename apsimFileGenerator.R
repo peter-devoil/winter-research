71b137cf087cbf62a1ecc6e52c42176506d2aa7d
Linden Wells <lindenwells9@gmail.com>
2021-07-10 18:17:01 +1000

    Modify apsimfileGen.R to update clock etc, add new Template based off fallow simulation in APSIM tutorial

--- apsimFileGenerator.R
+++ apsimFileGenerator.R
@@ -1,10 +1,10 @@
 library("XML")
-for (filename in list.files("Example_Met_Files")) {
-  
+library("lubridate")
+
+# Helper function to pad a day or month out to 2 digits when applicable
+pad <- function(num) {
+  return(sprintf("%02d", num))
 }
-
-sites <- read.csv("wrp/progress/Allsites.csv")
-
 
 # Expects a .met file with name YYYYMMDD.Site.emember#.met and a .apsim template
 # Will do nothing if an apsim file with the same name as metfile is already there. 
@@ -23,7 +23,15 @@ apsimFileGen <- function(metFileName, templateFileName) {
     file.copy(from = templateFileName, to = outFN)
     doc <- xmlParse(outFN)
     for (node in getNodeSet(doc, "//filename[@name='filename' and @input='yes']")) {
-      xmlValue(node) <- outFN
+      xmlValue(node) <- paste(date, site, emember, "met", sep=".")
+    }
+    for (node in getNodeSet(doc, "//clock/start_date")) {
+      x <- ymd(date) + days(10)
+      xmlValue(node) <- paste0(pad(day(x)), '/', pad(month(x)), '/', year(x))
+    }
+    for (node in getNodeSet(doc, "//clock/end_date")) {
+      x <- ymd(date) + days(60)
+      xmlValue(node) <- paste0(pad(day(x)), '/', pad(month(x)), '/', year(x))
     }
     for (node in getNodeSet(doc, "//filename[@output='yes']")) {
       xmlValue(node) <- paste(date, site, emember, "out", sep=".")
@@ -34,11 +42,18 @@ apsimFileGen <- function(metFileName, templateFileName) {
     for (node in getNodeSet(doc, "//ui/emember")) {
       xmlValue(node) <- emember
     }
-    
-    
+    for (node in getNodeSet(doc, "//outputfile/filename")) {
+      xmlValue(node) <- paste(date, site, emember, "out", sep=".")
+    }
+    for (node in getNodeSet(doc, "//outputfile/title")) {
+      xmlValue(node) <- paste(date, site, emember, sep=".")
+    }
     
     saveXML(doc, outFN)
     return(TRUE)
   }
 }
 
+for (filename in list.files(pattern = "*.met")) {
+  apsimFileGen(filename, "Template.apsim")
+}
