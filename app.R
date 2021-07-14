35c2da7895eaeece5ba32acd80eeafebaba01dfd
Linden Wells <lindenwells9@gmail.com>
2021-07-14 16:06:15 +1000

    Shrink and centre the input panel. Change plot description. Remove spurious lines from plot_ribbon

--- app.R
+++ app.R
@@ -10,12 +10,9 @@ plot_ribbon <- function(df, date, siteLoc, period) {#period :: Int, site :: Stri
   filter(df, between(Date, date, date + days(period)), site == siteLoc) %>%
     ggplot() +
     geom_smooth(stat = 'summary', alpha = 0.5, fill = "gray", mapping = aes(Date, soil_mint_1),
-                fun.data = median_hilow, fun.args = list(conf.int = 0.5)) +
-    xlab("days from start") +
+                fun.data = median_hilow, fun.args = list(conf.int = 0.5)) + #conf.int 0.5 should be the IQR...
     
     ylab("minimum soil temperature (°C)") +
-    
-    scale_x_date() +
     
     theme_minimal()
 }
@@ -24,19 +21,20 @@ plot_ribbon <- function(df, date, siteLoc, period) {#period :: Int, site :: Stri
 ui <- fluidPage(h1("Predicting Soil Temps"),
                 
                 wellPanel(
+                  tags$head(tags$style(type='text/css', ".well {max-width: 300px;
+                                                                margin: auto;}")),
                   selectInput("site", label="Choose a site",
                               unique(ddf$site),
-                              #c("breeza", "dalby", "dubbo","emerald", "moree", "surat", "warra")
                   ),
                   dateInput("date", label="Choose a date to start at"),
-                  radioButtons("period", "Forecast for how many days?",
+                  radioButtons("period", "Forecast for how long?",
                                choiceNames = c("1 week", "2 weeks", "3 weeks", "4 weeks"),
                                choiceValues = c(7, 14, 21, 30))
                 ),
                 
                 plotOutput("plot"),
                 p("Blue line is the median of predicted soil min temp"),
-                p("Grey area is the interquartile range of predicted soil min temp"),
+                p("Grey area is the median 75% of predicted soil min temp"),
                 
                 tags$footer(
                   tags$a(href = "https://github.com/lindenwells/winter-research", "View this project on GitHub"))
