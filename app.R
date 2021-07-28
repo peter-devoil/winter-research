f0fdcdf95c3b5057a848cf389684829831199eff
peter de voil <peter.devoil@gmail.com>
2021-07-28 14:47:05 +1000

    Reactive exprs 3

--- app.R
+++ app.R
@@ -4,6 +4,7 @@ library(lubridate)
 library(Hmisc)
 library(leaflet)
 
+dbg<- function(...) cat(sprintf(...), sep='', file=stderr())
 
 sites<-read.csv("EarlySowingSites.csv", stringsAsFactors = F)
 
@@ -14,10 +15,10 @@ lastUpdateTime <- (file.info(files) %>% filter(mtime == max(mtime)))$mtime
 forecastDates <- as.Date(gsub(".RData", "", gsub("ddf.","", files, fixed=T), fixed=T))
 
 minForecastDate <- min(forecastDates)
-maxForecastDate <- max(forecastDates)
-
+forecastDate <- maxForecastDate <- max(forecastDates)
 
 ddf.hindcast <- readRDS("ddf.RData") # hindcast data ('1990:12)
+dbg(paste0("read ", nrow(ddf.hindcast), " lines from ddf.RData\n"))
 
 ui <- fluidPage(
   h3("Soil Temperature Prediction"),
@@ -30,11 +31,11 @@ ui <- fluidPage(
                                                                 margin: auto;}")),
         leafletOutput("map", height=300),
         selectInput("site", label="Site", sites),
-        dateInput("date", label="Choose a date to start at", 
-                  format = "dd/mm/yy",
-                  min= minForecastDate, 
-                  max= maxForecastDate,
-                  value = maxForecastDate),
+#        dateInput("date", label="Choose a date to start at", 
+#                  format = "dd/mm/yy",
+#                  min= minForecastDate, 
+#                  max= maxForecastDate,
+#                  value = maxForecastDate),
         radioButtons("period", "Forecast for how long?",
                      choiceNames = c("2 weeks", "4 weeks", "6 weeks"),
                      choiceValues = c(14, 28, 42),
@@ -75,20 +76,44 @@ ui <- fluidPage(
 
 
 server <- function(input, output, session) {
-  
   observeEvent(input$map_marker_click, { 
     p <- input$map_marker_click
     if (!is.null(p$id)) {
       updateSelectInput(session, "site", selected = p$id)
     }
   })
+
+  thisYear <- reactive({return(format.Date(forecastDate, "%Y"))})
+  
+  forecast <- reactive({
+    # The most recent file of forecast data
+    inputFile<- paste0("ddf.", forecastDate, ".RData")
+    ddf <- readRDS(inputFile) # Daily Data Frame which has forecast data
+    dbg(paste0("read ", nrow(ddf), " lines from ", inputFile, "\n"))
+    return(ddf[ddf$site ==  input$site & between(ddf$Date, forecastDate, forecastDate + days(input$period)),])
+  })
+  
+  hindcast <- reactive({
+    hcDates <- as.character(unique(ddf.hindcast$sowdate))
+    theseDates <- paste( hcDates, thisYear(), sep = "-")
+    hcDates <- sort(as.Date(theseDates, format="%d-%b-%Y"))
+    x <- forecastDate - hcDates
+    hcDate <- tolower(format.Date(hcDates[ which.min(x[ x > 0]) ], format="%d-%b"))
+    dbg(paste0("hindcast date = ", hcDate,"\n"))
+    
+    return(ddf.hindcast %>% 
+             filter(site == tolower(input$site) & src == "Pred") %>%
+             filter(sowdate == hcDate ) %>% 
+             mutate(dateNorm = as.Date(paste0(format.Date(Date, "%d/%m"),
+                                       "/", format.Date(forecastDate, "%Y")), format="%d/%m/%Y")) %>%
+             filter(between(dateNorm, forecastDate, forecastDate + days(input$period))))
+  })
   
   output$plot <- renderPlot({
-    hc <- hindcast()
     fc <- forecast()
-    #print(paste0("hindcast date =", hcDate, ", n=",nrow(hc)))
+    hc <- hindcast()
     ggplot() +
-      geom_boxplot(data=hc, aes(x=dateNorm, y=soil_mint_1, group=dateNorm), outlier.shape = NA)  +
+      geom_boxplot(data = hc, aes(x=dateNorm, y=soil_mint_1, group=dateNorm), outlier.shape = NA)  +
       geom_smooth(data = fc, stat = 'summary', alpha = 0.65, fill = "gray",
                   mapping = aes(Date, soil_mint_1),
                   fun.data = median_hilow, fun.args = list(conf.int = 0.5)) + #conf.int 0.5 should be the IQR...
@@ -97,8 +122,6 @@ server <- function(input, output, session) {
            y="Minimum Soil Temperature (°C)",
            x="") +
       theme_minimal()
-    
-#    plot_ribbon(input$date, input$site, input$period)
   })
   
   output$map <- renderLeaflet({
@@ -108,26 +131,6 @@ server <- function(input, output, session) {
       ) %>%
       addMarkers(data = sites, ~long, ~lat, label=~Name, layerId=~Name,
                  labelOptions = labelOptions(noHide = T, textOnly = TRUE))
-  })
-  
-  forecast <- reactive({
-    # The most recent file of forecast data
-    ddf <- readRDS(paste0("ddf.", input$date, ".RData")) # Daily Data Frame which has forecast data
-    return(ddf[ddf$site ==  input$site & between(ddf$Date, input$date, input$date + days(input$period)),])
-  })
-  
-  hindcast <- reactive({
-    hcDates <- unique(ddf.hindcast$sowdate)
-    hcDates <- sort(as.Date(paste( hcDates, format.Date(input$date, "%Y"), sep = "-"), format="%d-%b-%Y"))
-    x <- input$date - hcDates
-    hcDate <- tolower(format.Date(hcDates[ which.min(x[ x > 0]) ], format="%d-%b"))
-    #print(paste0("hindcast date = ", hcDate))
-    
-    return(filter (ddf.hindcast, site == tolower(input$site) & src == "Pred") %>%
-           filter (sowdate == hcDate ) %>% 
-           mutate(dateNorm = as.Date(paste0(format.Date(Date, "%d/%m"),
-                                       "/", format.Date(input$date, "%Y")), format="%d/%m/%Y")) %>%
-           filter(between(dateNorm, input$date, input$date + days(input$period))))
   })
 }
 
